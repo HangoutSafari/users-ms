a1669a275e26af1d0f465a881497b8c56f37fe27
Simon-Manasse <simo0047@hz.nl>
2023-12-22 16:54:27 +0100

    added neccessary microservices

--- controllers/usersViaSupabase.js
+++ controllers/usersViaSupabase.js
@@ -1,5 +1,4 @@
-
-import { getUsersData, getUserIdData, getAnimalsByUserId, handleUser, getFriendsForUser } from "../adapters/supabaseAdapter.js";
+import { getUsersData, getUserIdData, getAnimalsByUserId, handleUser, getFriendsForUser, getEventsOfUser, getEvent } from "../adapters/supabaseAdapter.js";
 export async function getUsers(req, res) {
   try {
     const data = await getUsersData();
@@ -34,30 +33,48 @@ export async function getFriends(req, res) {
   try {
     const userId = parseInt(req.params.userId);
     const friendsData = await getFriendsForUser(userId);
-    let detailedFriends = {'id':null}
-    if(friendsData.friends != null){
+    let detailedFriends = { 'id': null }
+    if (friendsData.friends != null) {
 
       const friendKeys = Object.keys(friendsData.friends);
-      
+
       detailedFriends = await Promise.all(
         friendKeys.map(async (friendKey) => await getUserIdData(friendKey))
-        );
-      }
-     
+      );
+    }
+
     res.json(detailedFriends);
   } catch (error) {
     res.status(500).send(error.message);
   }
 }
 
+
 export async function postAuthDetails(req, res) {
   const userData = req.body;
-  try{
+  try {
     const value = await handleUser(userData)
     res.status(200).json({ message: "Registration successful", data: value })
   }
   catch (err) {
     res.status(500).send(err.message);
   }
- };
+};
+
+
+export async function getUserEvents(req, res) {
+  try {
+    const userId = parseInt(req.params.userId);
+    const userEvents = await getEventsOfUser(userId);
+
+    for (let i = 0; i < userEvents.length; i++) {
+      const event = await getEvent(userEvents[i].event_id);
+      userEvents[i].event_id = event;
+    }
+
+    res.json(userEvents);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
